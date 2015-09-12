e181f72f86b938b19e659295b238077d199c7e11
pjsinco <psinco@fas.harvard.edu>
2015-09-12 12:17:58 -0500

    Rejigger search() on PhysiciansController

--- app/Http/Controllers/PhysiciansController.php
+++ app/Http/Controllers/PhysiciansController.php
@@ -46,67 +46,6 @@ class PhysiciansController extends ApiController
      */
     public function search(Request $request)
     {
-
-        /**
-         * Example queries
-         *   
-         * ?city=Grand+Rapids
-         * &state=MI
-         * &zip=
-         * &lat=42.961458
-         * &lon=-85.661874
-         * &what=
-         * &location=Grand+Rapids%2C+MI
-         * 
-         * ?city=Buckingham
-         * &state=IL
-         * &zip=60917
-         * &lat=41.044008
-         * &lon=-88.170607
-         * &what=
-         * &location=Buckingham%2C+IL+60917
-         * 
-         * ?city=Batavia
-         * &state=IL
-         * &zip=
-         * &lat=41.845761
-         * &lon=-88.31066
-         * &what=
-         * &location=Batavia%2C+IL
-         * 
-         * ?city=Naperville
-         * &state=IL
-         * &zip=60563
-         * &lat=41.792577
-         * &lon=-88.165647
-         * &what=
-         * &location=Naperville%2C+IL+60563
-         * 
-         * ?city=Grand+Rapids
-         * &state=MI
-         * &zip=
-         * &lat=42.961458
-         * &lon=-85.661874
-         * &what=Pediatrics
-         * &location=Grand+Rapids%2C+MI
-         * 
-         * ?city=Freehold
-         * &state=NY
-         * &zip=
-         * &lat=42.377411
-         * &lon=-74.056683
-         * &what=
-         * &location=Freehold%2C+NY
-         *
-         * ?city=Washington
-         * &state=DC
-         * &zip=20221
-         * &lat=38.894103
-         * &lon=-77.028884
-         * &what=Pediatrics
-         * &location=Washington%2C+DC+20221
-         *
-         */
         $distance = 25;
         $haversineSelect  = "*, (3959 * acos( cos( radians(" . $request->lat;
         $haversineSelect .= ") ) * cos( radians( lat ) ) * ";
@@ -116,8 +55,7 @@ class PhysiciansController extends ApiController
 
         //if ($request->ajax()) {
 
-            if ($request->has('lat') && $request->has('lon') && $request->has('name')) {
-
+            if (!$request->has('s_code')) {
                 DB::setFetchMode(\PDO::FETCH_ASSOC);
                 $physicians = DB::table('physicians')
                     ->select(DB::raw($haversineSelect))
@@ -128,65 +66,32 @@ class PhysiciansController extends ApiController
                     ->get();
                 DB::setFetchMode(\PDO::FETCH_CLASS);
 
-                if (! empty($physicians)) {
-                    return $this->respond([
-                        'data' => 
-                            $this->physicianTransformer->transformCollection($physicians)
-                    ]);
-                }
+            } else  {
+                DB::setFetchMode(\PDO::FETCH_ASSOC);
+                $physicians = DB::table('physicians')
+                    ->select(DB::raw($haversineSelect))
+                    ->where('PrimaryPracticeFocusCode', '=', $request->s_code )
+                    ->having('distance', '<', $distance)
+                    ->orderBy('distance', 'asc')
+                    ->get();
+                    DB::setFetchMode(\PDO::FETCH_CLASS);
 
-                return Response::json([
-                    'error' => [
-                        'message' => 'No physicians found',
-                        'status_code' => 404,
-                    ]
-                ], 404);
+            } 
+            if (! empty($physicians)) {
+                return $this->respond([
+                    'data' => $this->physicianTransformer
+                                   ->transformCollection($physicians)
+                ]);
             }
+
+            return Response::json([
+                'error' => [
+                    'message' => 'No physicians found',
+                    'status_code' => 404,
+                ]
+            ], 404);
         //}
 
-        /**
-         * "city" => "Long Pine"
-         * "state" => "NE"
-         * "zip" => "69217"
-         * "lat" => "42.436196"
-         * "lon" => "-99.750801"
-         * "s_code" => "OR"
-         * "specialty" => "Orthopedics"
-         * "location" => "Long Pine, NE 69217"
-         * 
-         * "city" => "Nakina"
-         * "state" => "NC"
-         * "zip" => "28455"
-         * "lat" => "34.112441"
-         * "lon" => "-78.653824"
-         * "s_code" => ""
-         * "specialty" => ""
-         * "location" => "Nakina, NC 28455"
-         */
 
-        //DB::setFetchMode(\PDO::FETCH_ASSOC);
-        if ($request->has('s_code')) {
-            $physicians = DB::table('physicians')
-                ->select(DB::raw($haversineSelect))
-                ->where('PrimaryPracticeFocusCode', '=', $request->s_code )
-                ->having('distance', '<', $distance)
-                ->orderBy('distance', 'asc')
-                ->get();
-        } else {
-            $physicians = DB::table('physicians')
-                ->select(DB::raw($haversineSelect))
-                ->having('distance', '<', $distance)
-                ->orderBy('distance', 'asc')
-                ->get();
-        }
-        //DB::setFetchMode(\PDO::FETCH_CLASS);
-
-        return view('search.results')
-            ->with('physicians', $physicians)
-            ->with('city', $request->city)
-            ->with('state', $request->state)
-            ->with('specialty', $request->specialty);
-        
-        
     }
 }
