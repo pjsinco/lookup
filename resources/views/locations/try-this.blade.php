d6fa6bb84eded557b87f4187632b655f44c582c3
pjsinco <psinco@fas.harvard.edu>
2015-09-19 16:10:55 -0500

    Tweak style

--- resources/views/locations/try-this.blade.php
+++ resources/views/locations/try-this.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-xs-12">
-            <h1 style="margin-top: 1em; font-weight: 200; font-size: 12em; line-height: .8; letter-spacing: -9px; color: #8f8f8f" class="text-center">{{ $location['city'] }}, {{ $location['state'] }} <small style="font-size: 45%; color: #f5f5f5; letter-spacing: -5px;">{{ $location['zip'] }}</small></h1>
+            <h1 style="margin-top: 1em; font-weight: 200; font-size: 12em; line-height: .8; letter-spacing: -5px; color: #8f8f8f" class="text-center">{{ $location['city'] }}, {{ $location['state'] }} <small style="font-size: 45%; color: #cacaca; letter-spacing: -5px;">{{ $location['zip'] }}</small></h1>
     </div>
 </div>
 @stop
